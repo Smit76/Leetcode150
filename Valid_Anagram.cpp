40e1543e75c4c03f54f451e9f856e46870bd8cd9
smitrajsinh sarvaiya <smitrajsinhsarvaiya@smitrajsinhs-MacBook-Pro.local>
2023-04-30 16:49:20 -0700

    second approch

--- Valid_Anagram.cpp
+++ Valid_Anagram.cpp
@@ -20,3 +20,36 @@ public:
         return false;
     }
 };
+
+// another approch using hash maps
+
+class Solution
+{
+public:
+    bool isAnagram(string s, string t)
+    {
+
+        if (s.length() != t.length())
+        {
+            return false;
+        }
+        unordered_map<char, int> smap;
+        unordered_map<char, int> tmap;
+
+        for (int i = 0; i < t.length(); i++)
+        {
+            smap[s[i]]++;
+            tmap[t[i]]++;
+        }
+
+        for (int i = 0; i < smap.size(); i++)
+        {
+            if (smap[i] != tmap[i])
+            {
+                return false;
+            }
+        }
+
+        return true;
+    }
+};
